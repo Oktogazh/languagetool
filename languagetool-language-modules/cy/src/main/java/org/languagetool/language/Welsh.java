d7c3c12017a064bf17754a05eb1b5482a419973f
Alan Kersaudy <alan.kersaudy@gmail.com>
2025-10-10 00:31:31 +0200

    add: new project "cy"
    Following instructions from https://dev.languagetool.org/adding-a-new-language.html

--- languagetool-language-modules/cy/src/main/java/org/languagetool/language/Welsh.java
+++ languagetool-language-modules/cy/src/main/java/org/languagetool/language/Welsh.java
@@ -1,1 +1,84 @@
-﻿
+/* LanguageTool, a natural language style checker 
+ * Copyright (C) 2011 Daniel Naber (http://www.danielnaber.de)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+
+package org.languagetool.language;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.languagetool.*;
+import org.languagetool.rules.*;
+import org.languagetool.rules.spelling.SpellingCheckRule;
+import org.languagetool.tagging.disambiguation.Disambiguator;
+import org.languagetool.tagging.disambiguation.rules.XmlRuleDisambiguator;
+import org.languagetool.tokenizers.*;
+
+import java.io.IOException;
+import java.util.*;
+
+/** 
+ * @author Alan Kersaudy
+ */
+public class Welsh extends Language {
+
+  @Override
+  public SentenceTokenizer createDefaultSentenceTokenizer() {
+    return new SRXSentenceTokenizer(this);
+  }
+
+  @Override
+  public String getName() {
+    return "Welsh";
+  }
+
+  @Override
+  public String getShortCode() {
+    return "cy";
+  }
+
+  @Override
+  public String[] getCountries() {
+    return new String[] {"GB"};
+  }
+
+  @Override
+  public Disambiguator createDefaultDisambiguator() {
+    return new XmlRuleDisambiguator(this);
+  }
+
+  @Override
+  public Contributor[] getMaintainers() {
+    return new Contributor[] { new Contributor("Alan Kersaudy") };
+  }
+
+  @Override
+  public List<Rule> getRelevantRules(ResourceBundle messages, UserConfig userConfig, Language motherTongue, List<Language> altLanguages) throws IOException {
+    return Arrays.asList(
+            new CommaWhitespaceRule(messages),
+            new DoublePunctuationRule(messages),
+            new UppercaseSentenceStartRule(messages, this),
+            new MultipleWhitespaceRule(messages, this),
+            new SentenceWhitespaceRule(messages)
+    );
+  }
+
+  @Override
+  public LanguageMaintainedState getMaintainedState() {
+    return LanguageMaintainedState.ActivelyMaintained;
+  }
+}
